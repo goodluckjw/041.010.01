8e9cec8c1b9294598d620503de7c22d5eed3c4f0
goodluckjw <chetera@naver.com>
2025-04-19 03:45:28 +0900

    Update law_editor_app.py

--- app/law_editor_app.py
+++ app/law_editor_app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import streamlit as st
 from processing.law_processor import get_law_list_from_api, get_highlighted_articles
 
